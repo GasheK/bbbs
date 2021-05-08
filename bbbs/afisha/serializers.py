4c4939f4552962aff8b85504b12e791b16640fd7
Denis Nikitin <gashek88@gmail.com>
2021-05-08 22:51:12 +0300

    main page

--- bbbs/afisha/serializers.py
+++ bbbs/afisha/serializers.py
@@ -4,6 +4,11 @@ from bbbs.afisha.models import Event
 
 
 class EventSerializer(serializers.ModelSerializer):
+    booked = serializers.SerializerMethodField('get_booked')
+
+    def get_booked(self):
+        ...
+
     class Meta:
         model = Event
         fields = serializers.ALL_FIELDS
