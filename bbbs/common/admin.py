bb1934a9448b05847d5a19adcf5b18f9a4930649
Анна Полякова <Sova040888@yandex.ru>
2021-05-27 17:36:43 +0300

    Change model User

--- bbbs/common/admin.py
+++ bbbs/common/admin.py
@@ -3,29 +3,19 @@ from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import gettext_lazy as _
 
 from bbbs.common.mixins import DynamicLookupMixin
-from bbbs.common.models import City, Profile, User
+from bbbs.common.models import City, User
 from django.contrib.auth.models import Group
 
 
-class UserInline(admin.StackedInline):
-    empty_value_display = '-пусто-'
-    model = Profile
-    can_delete = False
-    verbose_name_plural = 'Доп. информация'
-
-
 class CustomUserAdmin(DynamicLookupMixin, UserAdmin):
-    list_display = ('username', 'is_active',
-                    'profile__role', 'profile__city', 'profile__curator')
-    search_fields = ('username', 'is_active',)
-    list_filter = ('is_active', 'profile__role', 'profile__city',)
-    profile__role_short_description = 'роль'
-    profile__city_short_description = 'город'
-    profile__curator_short_description = 'email куратора'
-    inlines = (UserInline,)
+    list_display = ('username', 'is_active', 'role')
+    search_fields = ('username', 'is_active', 'city',)
+    list_filter = ('is_active', 'role', 'city',)
+    filter_horizontal = ('city',)
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
-        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'role',
+                                         'city')}),
         (_('Permissions'), {
             'fields': ('is_active',),
         }),
@@ -34,9 +24,10 @@ class CustomUserAdmin(DynamicLookupMixin, UserAdmin):
 
     def get_form(self, request, obj=None, **kwargs):
         form = super().get_form(request, obj, **kwargs)
-        form.base_fields["username"].help_text = (
-            'В качестве имени укажите email пользователя'
-        )
+        if request.user == User.ADMIN:
+            form.base_fields["username"].help_text = (
+                'В качестве имени укажите email пользователя'
+            )
         disabled_fields = (
             'date_joined',
             'last_login',
@@ -56,6 +47,4 @@ class CityAdmin(admin.ModelAdmin):
 admin.site.register(City, CityAdmin)
 # Unregister Group
 admin.site.unregister(Group)
-# Unregister the provided model admin
-admin.site.unregister(User)
 admin.site.register(User, CustomUserAdmin)
--- bbbs/common/permission.py
+++ bbbs/common/permission.py
@@ -1,4 +1,4 @@
-from bbbs.common.models import Profile
+from bbbs.common.models import User
 from rest_framework.permissions import SAFE_METHODS, BasePermission
 from django.contrib.auth.models import PermissionsMixin
 
@@ -6,46 +6,53 @@ from django.contrib.auth.models import PermissionsMixin
 class ModulePermissionMixin:
 
     def has_module_permission(self, request):
-        return True
+        if request.user.is_authenticated:
+            allowed_user_roles = [User.ADMIN, User.MODERATOR_GENERAL]
+            if request.user.role in allowed_user_roles:
+                return True
+            return False
+        return False
 
 
 class ViewPermissionMixin:
 
     def has_view_permission(self, request, obj=None):
-        if request.user.profile.role == Profile.MENTOR:
-            return False
-        return True
+        allowed_user_roles = [User.ADMIN, User.MODERATOR_GENERAL]
+        if request.user.role in allowed_user_roles:
+            return True
+        return False
+
 
 
 class AddPermissionMixin:
 
     def has_add_permission(self, request):
-        if request.user.profile.role != Profile.ADMIN:
-            return False
-        return True
+        allowed_user_roles = [User.ADMIN, User.MODERATOR_GENERAL]
+        if request.user.role in allowed_user_roles:
+            return True
+        return False
 
 
 class ChangePermissionMixin:
 
     def has_change_permission(self, request, obj=None):
-        if request.user.profile.role != Profile.MENTOR:
+        allowed_user_roles = [User.ADMIN, User.MODERATOR_GENERAL]
+        if request.user.role in allowed_user_roles:
             return True
-        return True
+        return False
 
 
 class DeletePermissionMixin:
 
     def has_delete_permission(self, request, obj=None):
-        if request.user.profile.role != Profile.ADMIN:
-            return False
-        return True
+        allowed_user_roles = [User.ADMIN, User.MODERATOR_GENERAL]
+        if request.user.role in allowed_user_roles:
+            return True
+        return False
 
 
-class EventAdminPermissionMixin(
+class UserAdminPermissionMixin(
     ModulePermissionMixin,
     ViewPermissionMixin,
-    AddPermissionMixin,
-    ChangePermissionMixin,
-    DeletePermissionMixin,
 ):
     pass
